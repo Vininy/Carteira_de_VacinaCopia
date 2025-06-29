45f4934d52f5358d0f677b5878a0223073671bc2
FelipeAlmadaa <felipealmada1403@yahoo.com>
2025-06-29 19:44:31 -0300

    correção nomeclatura ID

--- back-end/models/associations.js
+++ back-end/models/associations.js
@@ -2,14 +2,15 @@ import { User } from './User.js';
 import { Vaccine } from './Vaccine.js';
 import { UserVaccine } from './UserVaccine.js';
 
+// Associações many-to-many com nomes de chave corretos
 User.belongsToMany(Vaccine, {
   through: UserVaccine,
   foreignKey: 'user_id',
-  otherKey: 'vaccine_id'
+  otherKey: 'vacina_id'
 });
 
 Vaccine.belongsToMany(User, {
   through: UserVaccine,
-  foreignKey: 'vaccine_id',
+  foreignKey: 'vacina_id',
   otherKey: 'user_id'
 });
