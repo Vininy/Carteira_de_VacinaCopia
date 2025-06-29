81e271987f08e04580d1411b949af95799f9dbd9
FelipeAlmadaa <felipealmada1403@yahoo.com>
2025-06-29 19:33:08 -0300

    nome de coluna errada - correção

--- back-end/models/UserVaccine.js
+++ back-end/models/UserVaccine.js
@@ -2,8 +2,15 @@ import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/db.js';
 
 export const UserVaccine = sequelize.define('UserVaccine', {
-  aplicada: { type: DataTypes.BOOLEAN, defaultValue: false },
-  dataAplicacao: { type: DataTypes.DATEONLY, allowNull: true }
+  aplicada: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  dataAplicacao: {
+    type: DataTypes.DATEONLY,
+    allowNull: true,
+    field: 'data_aplicacao'  // aqui o mapeamento correto
+  }
 }, {
   tableName: 'vacinas_usuarios',
   freezeTableName: true,
