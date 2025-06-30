a908536480890014573294f41d2ff19678b6592a
FelipeAlmadaa <felipealmada1403@yahoo.com>
2025-06-29 21:15:36 -0300

    fix

--- back-end/models/associations.js
+++ back-end/models/associations.js
@@ -2,13 +2,15 @@ import { User } from './User.js';
 import { Vaccine } from './Vaccine.js';
 import { UserVaccine } from './UserVaccine.js';
 
+
 User.belongsToMany(Vaccine, {
   through: UserVaccine,
-  foreignKey: 'user_id',
-  otherKey: 'vaccine_id'
+  foreignKey: 'user_id',     
+  otherKey: 'vacina_id'       
 });
+
 Vaccine.belongsToMany(User, {
   through: UserVaccine,
-  foreignKey: 'vaccine_id',
-  otherKey: 'user_id'
+  foreignKey: 'vacina_id',    
+  otherKey: 'user_id'         
 });
